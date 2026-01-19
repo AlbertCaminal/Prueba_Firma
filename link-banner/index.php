276ab16111b93f8ddc12469d623644a673e74420
Albert Caminal <albertcaminalmallol@gmail.com>
2026-01-19 11:28:36 +0100

    php

---
+++ link-banner/index.php
@@ -0,0 +1,7 @@
+<?php
+// REDIRECCIÓN PARA LA FIRMA
+// Cambia la URL de abajo (entre las comillas) por el destino real
+
+header("Location: https://tresipunt.com/es/contenidos-elearning/gamificacion/");
+exit();
+?>
